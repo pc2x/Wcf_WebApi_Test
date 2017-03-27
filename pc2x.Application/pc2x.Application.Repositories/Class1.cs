2fe9b6dda738c74adeaee8943065a8253b95ec35
juanalberto.villegas <juanalberto.villegas@broxel.com>
2017-03-27 13:38:41 -0600

    se agregó EF al proyecto

---
+++ pc2x.Application/pc2x.Application.Repositories/Class1.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Data.Entity;
+
+namespace pc2x.Application.Repositories
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("ApplicationDbContext")
+        {
+
+        }
+    }
+}
