b1b8f43e721010dd9cb510f8d0da66e537e4c307
juanalberto.villegas <juanalberto.villegas@broxel.com>
2017-03-30 10:42:03 -0600

    se agregan domain models que estaban como untracked files

---
+++ pc2x.Application/pc2x.Application.Core/DomainModels/TipoAsentamientoModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace pc2x.Application.Core.DomainModels
+{
+    public class TipoAsentamientoModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ pc2x.Application/pc2x.Application.Repositories/Repositories/PaisRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using pc2x.Application.Core.DomainModels;
+using pc2x.Application.Core.ServicesContracts;
+using pc2x.Application.Repositories.EntityFramework;
+using pc2x.Application.Repositories.EntityFramework.Entities;
+using pc2x.Application.Repositories.Mappers;
+
+namespace pc2x.Application.Repositories.Repositories
+{
+    public class PaisRepository : GenericRepository<PaisEntity, PaisModel, ApplicationDbContext>, IPaisService
+    {
+        public PaisRepository()
+        {
+            AutoMapperConfiguration.Configure();
+        }
+    }
+}
